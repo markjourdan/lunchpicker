9f77fc25e4dec313a386aed4094b081eb245c217
Mark Jourdan <mjourdan@markjourdan.com>
2013-01-13 21:18:32 -0600

    Added jquery Added some jquery plugins for the navigation, and the rounded corner effects Added Kendo UI Added BundleConfig Added the error page Added the navigation template Added the layout for testing
    Made some modifications for the view on the home page

---
+++ src/Website/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace LunchPicker.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery.*"));
+            
+            bundles.Add(new ScriptBundle("~/Scripts/jquery-rumble").Include(
+                "~/Scripts/jrumble.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/Site.css"));
+        }
+    }
+}
---
+++ src/Website/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace LunchPicker.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RestaurantRanking()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

0e022567b86959640bb26d48394e13bdc4d86799
Mark Jourdan <mjourdan@markjourdan.com>
2013-01-20 21:57:36 -0600

    worked further on the Add Restaurant test

--- src/Domain/Entities/Restaurant.cs
+++ src/Domain/Entities/Restaurant.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Security.Principal;
 using System.Text;
 
--- src/Tests/Website/Areas/Clique/Restaurant/AddRestaurant.cs
+++ src/Tests/Website/Areas/Clique/Restaurant/AddRestaurant.cs
@@ -1,5 +1,12 @@
-﻿using LunchPicker.Web.Areas.Clique.Controllers;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Dino;
+using Kendo.Mvc.UI;
+using LunchPicker.Domain;
+using LunchPicker.Domain.Repositories;
+using LunchPicker.Web.Areas.Clique.Controllers;
 using NUnit.Framework;
+using Rhino.Mocks;
 
 namespace Tests.Website.Areas.Clique.Restaurant
 {
@@ -7,11 +14,18 @@ namespace Tests.Website.Areas.Clique.Restaurant
     public class AddRestaurant
     {
         private RestaurantController _controller;
-
+        private ILunchRepository _lunchRepository;
+        private ControllerContext _controllerContext;
+        private ISession _session;
         [SetUp]
         public void Setup()
         {
-            _controller = new RestaurantController();
+            _lunchRepository = MockRepository.GenerateMock<ILunchRepository>();
+            _session = MockRepository.GenerateMock<ISession>();
+            _controllerContext = MockRepository.GenerateMock<ControllerContext>();
+            _controllerContext.Stub(s => s.HttpContext.User.Identity.Name).Return("User");
+
+            _controller = new RestaurantController { Clock = new Clock(), LunchRepository = _lunchRepository, ControllerContext = _controllerContext, _Session = _session};
         }
 
         [TearDown]
@@ -21,9 +35,16 @@ namespace Tests.Website.Areas.Clique.Restaurant
         }
 
         [Test]
+        [Ignore]
         public void SuccessfullyAdd()
         {
+            _controller.ViewData.ModelState.Clear();
 
+            var restaraunt = new LunchPicker.Domain.Entities.Restaurant();
+
+            _controller.AddRestaurant(new DataSourceRequest(), restaraunt);
+
+            Assert.IsTrue(_controller.ViewData.ModelState.Any());
         }
     }
 }

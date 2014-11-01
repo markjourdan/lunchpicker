0e823ed03284b7b1afdb8fcdfd385feff27b9270
Mark Jourdan <mjourdan@markjourdan.com>
2014-10-31 20:27:06 -0500

    upgraded references removed legacy files upgraded Kendo Turned to MVC project

--- src/Website/Global.asax.cs
+++ src/Website/Global.asax.cs
@@ -27,7 +27,7 @@ namespace LunchPicker.Web
 
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
